2af2c1952dee34769af5b00b38c32aee29322eb2
Herman Crawfurd Svensen <herman@scel.to>
2016-02-02 12:22:14 +0100

    Added props on Command.

--- Herms.Cqrs/Command.cs
+++ Herms.Cqrs/Command.cs
@@ -2,5 +2,10 @@
 
 namespace Herms.Cqrs
 {
-    public class Command {}
+    public class Command
+    {
+        public Guid Id { get; set; }
+        public Guid CorrelationId { get; set; }
+        public Guid AggregateId { get; set; }
+    }
 }

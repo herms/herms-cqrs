2f299ce0822062684840f66b3bc70bded8138e43
Herman Crawfurd Svensen <herman@scel.to>
2016-02-12 15:08:03 +0100

    Added AggregateId and Id to IEvent.

--- Herms.Cqrs/Event/IEvent.cs
+++ Herms.Cqrs/Event/IEvent.cs
@@ -4,6 +4,8 @@ namespace Herms.Cqrs.Event
 {
     public interface IEvent
     {
+        Guid Id { get; }
+        Guid AggregateId { get; }
         int Version { get; }
         DateTime Timestamp { get; }
     }

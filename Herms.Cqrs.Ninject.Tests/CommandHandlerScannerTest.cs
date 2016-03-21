0325cd9f60f89a5d9ed92269a6e74e8ba3b84fb5
Herman Crawfurd Svensen <herman@scel.to>
2016-03-21 15:49:39 +0100

    Hmm. Forgot to save before last commit.

--- Herms.Cqrs.Ninject.Tests/CommandHandlerScannerTest.cs
+++ Herms.Cqrs.Ninject.Tests/CommandHandlerScannerTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 using Herms.Cqrs.Ninject.Tests.CommandHandlers;
 using Herms.Cqrs.Ninject.Tests.Commands;
-using Herms.Cqrs.Ninject.Tests.Events;
 using Ninject;
 using Xunit;
 
@@ -31,32 +29,6 @@ namespace Herms.Cqrs.Ninject.Tests
             Assert.Equal(handlerForTestCommand1.GetType(), typeof (TestCommandHandler1));
             Assert.Equal(handlerForTestCommand2.GetType(), typeof (TestCommandHandler2));
             Assert.Equal(handlerForTestCommand3.GetType(), typeof (TestCommandHandler2));
-        }
-    }
-
-    public class EventHandlerScannerTest
-    {
-        [Fact]
-        public void GivenEventHandlersInAssembly_WhenScanning_ThenHandlersShouldBeRegistered()
-        {
-            var kernel = new StandardKernel();
-            var scanner = new EventHandlerScanner(kernel);
-
-            scanner.ScanAndRegisterEventHandlers(GetType().Assembly);
-
-            var eventHandlerRegistry = new NinjectEventHandlerRegistry(kernel);
-
-            var handlersForTestEvent1 = eventHandlerRegistry.ResolveHandlers(new TestEvent1());
-            var handlersForTestEvent2 = eventHandlerRegistry.ResolveHandlers(new TestEvent2());
-            var handlersForTestEvent3 = eventHandlerRegistry.ResolveHandlers(new TestEvent3());
-
-            Assert.NotNull(handlersForTestEvent1);
-            Assert.NotNull(handlersForTestEvent2);
-            Assert.NotNull(handlersForTestEvent3);
-
-            Assert.Equal(1, handlersForTestEvent1.Count());
-            Assert.Equal(2, handlersForTestEvent2.Count());
-            Assert.Equal(1, handlersForTestEvent3.Count());
         }
     }
 }

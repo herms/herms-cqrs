9ce2c1160811c235bb8944a310c79527081ef1f5
Herman Crawfurd Svensen <herman@scel.to>
2016-03-23 18:52:37 +0100

    Made static class that can look at generic interfaces and extract generic argument, e.g. find all events that an implementation of IEventHandler<> can handle.

---
+++ Herms.Cqrs/GenericArgumentExtractor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Herms.Cqrs.Event;
+
+namespace Herms.Cqrs
+{
+    public static class GenericArgumentExtractor
+    {
+        public static List<Type> GetApplicableEvents(Type implementationType)
+        {
+            return GetGenericArgumentsFromInterface(implementationType, typeof (IApplyEvent<>), typeof (IEvent));
+        }
+
+        public static List<Type> GetHandledEvents(Type implementationType)
+        {
+            return GetGenericArgumentsFromInterface(implementationType, typeof (IEventHandler<>), typeof (IEvent));
+        }
+
+        private static List<Type> GetGenericArgumentsFromInterface(Type implementation, Type genericInterface, Type genericBaseType)
+        {
+            var types = new List<Type>();
+            var handlerInterfaces =
+                implementation.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == genericInterface);
+            foreach (var handlerInterface in handlerInterfaces)
+            {
+                var genericArguments = handlerInterface.GetGenericArguments();
+                if (genericArguments.Length == 1 && genericBaseType.IsAssignableFrom(genericArguments[0]))
+                {
+                    types.Add(genericArguments[0]);
+                }
+            }
+            return types;
+        }
+    }
+}

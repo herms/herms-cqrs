c592c4e572b31eeeb780b534cc202baa9a5ff87e
Herman Crawfurd Svensen <herman@scel.to>
2016-04-04 14:49:24 +0200

    Started on azure table storage for events. Added test project.

---
+++ Herms.Cqrs.Azure.Tests/AzureEventRepositoryTests.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace Herms.Cqrs.Azure.Tests
+{
+    public class AzureEventRepositoryTests {}
+}
--- Herms.Cqrs.Azure/AzureEventRepository.cs
+++ Herms.Cqrs.Azure/AzureEventRepository.cs
@@ -9,13 +9,16 @@ namespace Herms.Cqrs.Azure
 {
     public class AzureEventRepository<TAggregate> : IAggregateRepository<TAggregate> where TAggregate : IAggregate, IEventSourced, new()
     {
+        private readonly string _tableName;
         private readonly CloudTable _table;
+        private readonly string GuidStringFormat = "D";
 
         public AzureEventRepository(string connectionString, string tableName)
         {
+            _tableName = tableName;
             var storageAccount = CloudStorageAccount.Parse(connectionString);
             var tableClient = storageAccount.CreateCloudTableClient();
-            _table = tableClient.GetTableReference(tableName);
+            var cloudTable = _table = tableClient.GetTableReference(tableName);
         }
 
         public AzureEventRepository(string connectionString) : this(connectionString, typeof (TAggregate).Name) {}
@@ -25,7 +28,7 @@ namespace Herms.Cqrs.Azure
             var batch = new TableBatchOperation();
             foreach (var @event in aggregate.GetChanges())
             {
-                var eventEntity = new EventEntity(@event.AggregateId.ToString("D"), @event.EventId.ToString("D"))
+                var eventEntity = new EventEntity(@event.AggregateId.ToString(GuidStringFormat), @event.EventId.ToString(GuidStringFormat))
                 {
                     AggregateType = typeof (TAggregate).Name,
                     EventType = @event.GetType().Name,
@@ -37,12 +40,27 @@ namespace Herms.Cqrs.Azure
                 batch.Add(operation);
             }
             var result = _table.ExecuteBatch(batch);
-            if (result.Any(r => r.HttpStatusCode != 200)) {}
+            if (result.Any(r => r.HttpStatusCode != 200))
+            {
+                
+            }
+            
         }
 
         public TAggregate Get(Guid id)
         {
-            throw new NotImplementedException();
+            var query =
+                new TableQuery<EventEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal,
+                    id.ToString(GuidStringFormat)));
+            var results = _table.ExecuteQuery(query);
+            var eventEntities = results.ToList().OrderBy(e => e.Version);
+            if (eventEntities.Any())
+            {
+                var aggregate = new TAggregate();
+                aggregate.Load(eventEntities.Select(e => e.Payload));
+                return aggregate;
+            }
+            return default(TAggregate);
         }
     }
 
@@ -55,5 +73,7 @@ namespace Herms.Cqrs.Azure
         public IEvent Payload { get; set; }
 
         public EventEntity(string partitionKey, string rowKey) : base(partitionKey, rowKey) {}
+
+        public EventEntity() {}
     }
 }

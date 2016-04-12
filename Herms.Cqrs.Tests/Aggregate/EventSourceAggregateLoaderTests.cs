0e341a4d14e7ae5349d9b8d31635f2ed7efa3ff6
Herman Crawfurd Svensen <h@herms.no>
2016-04-12 16:54:34 +0200

    Made a static class for loading aggregates from event streams.

---
+++ Herms.Cqrs.Tests/Aggregate/EventSourceAggregateLoaderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Herms.Cqrs.Aggregate;
+using Herms.Cqrs.Event;
+using Herms.Cqrs.TestContext.Events;
+using Herms.Cqrs.TestContext.Models;
+using Xunit;
+
+namespace Herms.Cqrs.Tests.Aggregate
+{
+    public class EventSourceAggregateLoaderTests
+    {
+        [Fact]
+        public void GivenEventList_WhenLoadingAggregate_ThenAggregateStateIsRecreated()
+        {
+            var aggregate =
+                AggregateLoader.LoadFromEventStream<TestAggregate>(new List<IEvent>
+                {
+                    new TestEvent1 { Version = 1 },
+                    new TestEvent2 { Param1 = "A1-1" },
+                    new TestEvent2 { Param1 = "A1-2" }
+                });
+            Assert.NotNull(aggregate);
+            Assert.Equal("A1-2", aggregate.Prop1);
+        }
+    }
+}
---
+++ Herms.Cqrs/Aggregate/AggregateLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Herms.Cqrs.Event;
+
+namespace Herms.Cqrs.Aggregate
+{
+    public static class AggregateLoader
+    {
+        public static TAggregate LoadFromEventStream<TAggregate>(IEnumerable<IEvent> events)
+            where TAggregate : EventSourcedAggregateBase, IEventSourced, new()
+        {
+            var aggregate = new TAggregate();
+            aggregate.Load(events);
+            return aggregate;
+        }
+    }
+}

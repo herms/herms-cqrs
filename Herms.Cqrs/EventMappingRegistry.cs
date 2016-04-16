69c7b083f398c02a12331ba4cc9b4566f0ca3a9d
Herman Crawfurd Svensen <h@herms.no>
2016-04-16 20:59:56 +0200

    Created an event name/type registry.

---
+++ Herms.Cqrs/EventMappingRegistry.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Herms.Cqrs.Registration;
+
+namespace Herms.Cqrs
+{
+    public class EventMappingRegistry : Dictionary<string, Type>, IEventMappingRegistry
+    {
+        public void Register(EventMapping eventMapping)
+        {
+            this.Add(eventMapping.EventName, eventMapping.EventType);
+        }
+
+        public void Register(IEnumerable<EventMapping> eventMappings)
+        {
+            foreach (var eventMapping in eventMappings)
+            {
+                this.Register(eventMapping);
+            }
+        }
+
+        public Type ResolveEventType(string name)
+        {
+            return this[name];
+        }
+    }
+}
---
+++ Herms.Cqrs/IEventMappingRegistry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using Herms.Cqrs.Registration;
+
+namespace Herms.Cqrs
+{
+    public interface IEventMappingRegistry
+    {
+        void Register(EventMapping eventMapping);
+        void Register(IEnumerable<EventMapping> eventMappings);
+        Type ResolveEventType(string name);
+    }
+}

6ee50a9e9e40f1661377d5384dea46412cb50800
Herman Crawfurd Svensen <h@herms.no>
2016-09-15 14:50:49 +0200

    Support scanning several assemblies.

--- Herms.Cqrs.Ninject/CqrsNinjectModule.cs
+++ Herms.Cqrs.Ninject/CqrsNinjectModule.cs
@@ -7,36 +7,38 @@ namespace Herms.Cqrs.Ninject
 {
     public class CqrsNinjectModule : NinjectModule
     {
-        private readonly Assembly _assembly;
+        private readonly Assembly[] _assemblies;
         private readonly ICommandHandlerRegistry _commandHandlerRegistry;
         private readonly IEventHandlerRegistry _eventHandlerRegistry;
         private readonly IEventMappingRegistry _eventMappingRegistry;
 
-        public CqrsNinjectModule(Assembly assembly, ICommandHandlerRegistry commandHandlerRegistry)
+        public CqrsNinjectModule(Assembly[] assemblies, ICommandHandlerRegistry commandHandlerRegistry)
         {
-            _assembly = assembly;
+            _assemblies = assemblies;
             _commandHandlerRegistry = commandHandlerRegistry;
         }
 
-        public CqrsNinjectModule(Assembly assembly, ICommandHandlerRegistry commandHandlerRegistry,
+        public CqrsNinjectModule(Assembly[] assemblies, ICommandHandlerRegistry commandHandlerRegistry,
             IEventHandlerRegistry eventHandlerRegistry,
             IEventMappingRegistry eventMappingRegistry)
         {
-            _assembly = assembly;
+            _assemblies = assemblies;
             _commandHandlerRegistry = commandHandlerRegistry;
             _eventHandlerRegistry = eventHandlerRegistry;
             _eventMappingRegistry = eventMappingRegistry;
         }
 
-        public CqrsNinjectModule(Assembly assembly, IEventHandlerRegistry eventHandlerRegistry)
+        public CqrsNinjectModule(Assembly[] assemblies, IEventHandlerRegistry eventHandlerRegistry,
+            IEventMappingRegistry eventMappingRegistry)
         {
-            _assembly = assembly;
+            _assemblies = assemblies;
             _eventHandlerRegistry = eventHandlerRegistry;
+            _eventMappingRegistry = eventMappingRegistry;
         }
 
-        public CqrsNinjectModule(Assembly assembly, IEventMappingRegistry eventMappingRegistry)
+        public CqrsNinjectModule(Assembly[] assemblies, IEventMappingRegistry eventMappingRegistry)
         {
-            _assembly = assembly;
+            _assemblies = assemblies;
             _eventMappingRegistry = eventMappingRegistry;
         }
 
@@ -46,31 +48,33 @@ namespace Herms.Cqrs.Ninject
             var scanForCommandsHandlers = _commandHandlerRegistry != null;
             var scanForEventHandlers = _eventHandlerRegistry != null;
             var scanForEvents = _eventMappingRegistry != null;
-            var targetAssembly = _assembly;
-            AssemblyScanResult result;
-            if (scanForCommandsHandlers && scanForEventHandlers && scanForEvents)
+            foreach (var targetAssembly in _assemblies)
             {
-                result = assemblyScanner.ScanAssembly(targetAssembly);
-                _commandHandlerRegistry.Register(result.CommandHandlers);
-                _eventHandlerRegistry.Register(result.EventHandlers);
-                _eventMappingRegistry.Register(result.EventMap);
-            }
-            else
-            {
-                if (scanForCommandsHandlers)
+                AssemblyScanResult result;
+                if (scanForCommandsHandlers && scanForEventHandlers && scanForEvents)
                 {
-                    result = assemblyScanner.ScanAssemblyForCommandHandlers(targetAssembly);
+                    result = assemblyScanner.ScanAssembly(targetAssembly);
                     _commandHandlerRegistry.Register(result.CommandHandlers);
-                }
-                if (scanForEventHandlers)
-                {
-                    result = assemblyScanner.ScanAssemblyForEventHandlers(targetAssembly);
                     _eventHandlerRegistry.Register(result.EventHandlers);
-                }
-                if (scanForEvents)
-                {
-                    result = assemblyScanner.ScanAssemblyForEvents(targetAssembly);
                     _eventMappingRegistry.Register(result.EventMap);
+                }
+                else
+                {
+                    if (scanForCommandsHandlers)
+                    {
+                        result = assemblyScanner.ScanAssemblyForCommandHandlers(targetAssembly);
+                        _commandHandlerRegistry.Register(result.CommandHandlers);
+                    }
+                    if (scanForEventHandlers)
+                    {
+                        result = assemblyScanner.ScanAssemblyForEventHandlers(targetAssembly);
+                        _eventHandlerRegistry.Register(result.EventHandlers);
+                    }
+                    if (scanForEvents)
+                    {
+                        result = assemblyScanner.ScanAssemblyForEvents(targetAssembly);
+                        _eventMappingRegistry.Register(result.EventMap);
+                    }
                 }
             }
         }

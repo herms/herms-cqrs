146c776d7a5bb132228d6c582b22807aca00801e
Herman Crawfurd Svensen <herman@herms.no>
2016-11-10 22:27:32 +0100

    Added singularity exception for aggregates.

---
+++ Herms.Cqrs/Aggregate/Exceptions/AggregateSingularityException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Herms.Cqrs.Aggregate.Exceptions
+{
+    public class AggregateSingularityException : Exception
+    {
+        public AggregateSingularityException(IAggregate aggregate, string fieldViolated, string value, Exception innerException = null)
+            : base($"{aggregate.GetType().Name}[{aggregate.Id}] already exists with value {value} for {fieldViolated}.", innerException) {}
+    }
+}

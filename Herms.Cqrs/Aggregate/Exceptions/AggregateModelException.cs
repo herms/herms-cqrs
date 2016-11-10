c48e8f16452c59f5986884625449ac29597c7d6e
Herman Crawfurd Svensen <herman@herms.no>
2016-11-10 21:44:00 +0100

    Added model exception.

---
+++ Herms.Cqrs/Aggregate/Exceptions/AggregateModelException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Herms.Cqrs.Aggregate.Exceptions
+{
+    public class AggregateModelException : Exception
+    {
+        public AggregateModelException(Guid id, Type aggregateType, string message, Exception innerException = null)
+            : base(message, innerException)
+        {
+            Id = id;
+            AggregateType = aggregateType;
+        }
+
+        public Guid Id { get; }
+        public Type AggregateType { get; }
+    }
+}
